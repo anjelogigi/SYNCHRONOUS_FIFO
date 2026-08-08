da4c67dc68b71b24e980d76f5345efc1e06f212a
anjelogigi <anjelogigi@gmail.com>
2026-08-08 12:58:07 +0530

    Create fifo_test_pkg.sv

---
+++ SRC/tb/fifo_test_pkg.sv
@@ -0,0 +1,20 @@
+package fifo_test_pkg;
+ 
+ import uvm_pkg::*;
+`include "uvm_macros.svh"
+ 
+`include "fifo_trans.sv"
+`include "fifo_config.sv"
+`include "fifo_driver.sv"
+`include "fifo_input_monitor.sv"
+`include "fifo_output_monitor.sv"
+`include "fifo_sequencer.sv"
+`include "fifo_input_agent.sv"
+`include "fifo_output_agent.sv"
+`include "fifo_scoreboard.sv"
+`include "fifo_subscriber.sv"
+`include "fifo_env.sv"
+`include "fifo_seq.sv"
+`include "fifo_test.sv"
+ 
+endpackage
